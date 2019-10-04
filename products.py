a6d949f5d3de0afa575f0497ee1aaadb41fe188c
Dale-Peng <evildale727@gmail.com>
2019-10-04 23:34:35 +0800

    add check file list

--- products.py
+++ products.py
@@ -1,11 +1,17 @@
+import os
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('found')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('notfound')
 
 while True:
 	name = input('請輸入商品名稱:')
